f97e08ac164d36045aed7c33548597009b200274
Clive4216 <cliveesperance4216@gmail.com>
2025-02-12 12:10:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql_dbfirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolEntities())
+            {
+                var studentRepo = new Repository<Student>(context);
+                
+                // Display all students
+                Console.WriteLine("Students List:");
+                var students = studentRepo.GetAll();
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"Roll No: {student.roll_no}, Name: {student.name}");
+                }
+                Console.WriteLine();
+
+                // Insert new student
+                Console.WriteLine("Enter Student Roll No:");
+                int rollNo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Student Name:");
+                string name = Console.ReadLine();
+
+                var newStudent = new Student { roll_no = rollNo, name = name };
+                studentRepo.Add(newStudent);
+                studentRepo.Save();
+                Console.WriteLine("Student Added Successfully.\n");
+
+            }
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Sql_dbfirst
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SchoolEntities _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(SchoolEntities context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
